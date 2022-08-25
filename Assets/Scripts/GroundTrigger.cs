95e94f25c8678567e0956c99a623c0e06a03ebbf
okuda <okuda2107@gmail.com>
2022-08-25 16:54:08 +0900

    接地判定の追加

---
+++ Assets/Scripts/GroundTrigger.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTrigger : MonoBehaviour
+{
+
+private string groundTag = "Ground";
+private bool isGround = false;
+private bool isGroundEnter, isGroundStay, isGroundExit;
+
+//接地判定を返すメソッド
+//物理判定の更新毎に呼ぶ必要がある
+public bool IsGround()
+{
+   if(isGroundEnter || isGroundStay)
+   {
+      isGround = true;
+   }
+   else if(isGroundExit)
+   {
+      isGround = false;
+   } 
+
+   isGroundEnter = false;
+   isGroundStay = false;
+   isGroundExit = false;
+   return isGround; 
+}
+ 
+private void OnTriggerEnter2D(Collider2D collision)
+{
+   if (collision.tag == groundTag)
+   {
+      isGroundEnter = true;
+   }
+}
+ 
+private void OnTriggerStay2D(Collider2D collision)
+{
+   if (collision.tag == groundTag)
+   {
+      isGroundStay = true;
+   }
+}
+     
+private void OnTriggerExit2D(Collider2D collision)
+{
+   if (collision.tag == groundTag)
+   {
+      isGroundExit = true;
+   }
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
