b0d7e347df778f476e016ed11cbc4406930d3c92
okuda <okuda2107@gmail.com>
2022-08-25 17:22:25 +0900

    ダメージ床の判定を追加

--- Assets/Scripts/GroundTrigger.cs
+++ Assets/Scripts/GroundTrigger.cs
@@ -7,7 +7,9 @@ public class GroundTrigger : MonoBehaviour
 
 private string groundTag = "Ground";
 private bool isGround = false;
+private bool isDamageGround = false;
 private bool isGroundEnter, isGroundStay, isGroundExit;
+private bool isDamageGroundEnter, isDamageGroundStay, isDamageGroundExit;
 
 //接地判定を返すメソッド
 //物理判定の更新毎に呼ぶ必要がある
@@ -27,28 +29,63 @@ public bool IsGround()
    isGroundExit = false;
    return isGround; 
 }
+
+public bool IsDamageGround()
+{
+   if(isDamageGroundEnter || isDamageGroundStay)
+   {
+      isDamageGround = true;
+   }
+   else if(isDamageGroundExit)
+   {
+      isDamageGround = false;
+   } 
+
+   isDamageGroundEnter = false;
+   isDamageGroundStay = false;
+   isDamageGroundExit = false;
+   return isDamageGround; 
+}
  
 private void OnTriggerEnter2D(Collider2D collision)
 {
-   if (collision.tag == groundTag)
+   switch (collision.tag)
    {
+      case "Ground":
       isGroundEnter = true;
+      break;
+
+      case "DamageGround":
+      isDamageGroundEnter = true;
+      break;
    }
 }
  
 private void OnTriggerStay2D(Collider2D collision)
 {
-   if (collision.tag == groundTag)
+   switch (collision.tag)
    {
+      case "Ground":
       isGroundStay = true;
+      break;
+
+      case "DamageGround":
+      isDamageGroundStay = true;
+      break;
    }
 }
      
 private void OnTriggerExit2D(Collider2D collision)
 {
-   if (collision.tag == groundTag)
+   switch (collision.tag)
    {
+      case "Ground":
       isGroundExit = true;
+      break;
+
+      case "DamageGround":
+      isDamageGroundExit = true;
+      break;
    }
 }
 
